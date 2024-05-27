477f7635b339ed4b2d9044bd655886632a2e2056
Johnfen <42890585+Johnfen@users.noreply.github.com>
2024-05-27 14:22:22 +0800

    Update fts.c

--- drivers/input/touchscreen/fts_521/fts.c
+++ drivers/input/touchscreen/fts_521/fts.c
@@ -4953,7 +4953,7 @@ bool inline fts_touchmode_edgefilter(unsigned int touch_id, int x, int y)
 	return false;
 }
 
-int fts_read_touchmode_data()
+int fts_read_touchmode_data(void)
 {
 	int ret = 0;
 	u8 get_cmd[2] = {0xc1, 0x05};
@@ -5002,7 +5002,7 @@ int fts_read_touchmode_data()
 	return ret;
 }
 
-static void fts_init_touchmode_data()
+static void fts_init_touchmode_data(void)
 {
 	int i;
 
@@ -5124,7 +5124,7 @@ static void fts_edge_rejection(bool on, int value)
 	return;
 }
 
-static void fts_update_grip_mode()
+static void fts_update_grip_mode(void)
 {
 	int i, ret;
 	u8 grip_cmd[9] = {0xc0, 0x08, 0x00,};
@@ -5245,7 +5245,7 @@ static void fts_update_grip_mode()
 	return;
 }
 
-static void fts_update_touchmode_data()
+static void fts_update_touchmode_data(void)
 {
 	bool update = false;
 	int i, j, ret = 0;
